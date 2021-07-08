fb833c7e5a1c78c917dde634625fdd19d2239fc8
Jhen <79475798+jhen0605@users.noreply.github.com>
2021-07-08 17:52:54 +0800

    Initial Commit

---
+++ smallGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  smallGame
+//
+//  Created by 簡吟真 on 2021/7/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
